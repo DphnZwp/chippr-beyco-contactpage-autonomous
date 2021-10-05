db7c377f1e12a940b9d483208b235e1084a1ca84
DphnZwp <zwuupdaphne@gmail.com>
2021-10-05 16:37:01 +0200

    Animatie formulier
    Animatie voor formulier gemaakt en responsive. Design is nog niet af.

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+var card = document.getElementById('card');
+
+document.getElementById('flip').addEventListener('click', function() {
+    card.classList.toggle('flipped');
+}, false);
+
+document.getElementById('fli').addEventListener('click', function() {
+    card.classList.toggle('flipped');
+}, false);
